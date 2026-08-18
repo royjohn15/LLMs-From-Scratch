d93dbd578ff43656057784652b20aa1da0fc452e
royjohn15 <royjohnabc@gmail.com>
2026-08-18 13:15:15 +0530

    modified gitignore

--- Learning representations by backpropagating errors/main.py
+++ Learning representations by backpropagating errors/main.py
@@ -1,4 +1,17 @@
+from utils.activations import sigmoid, d_sigmoid, relu, d_relu, tanh, d_tanh
+
+
 def main():
+    x = 0.5
+    y = sigmoid(x)
+    dy = d_sigmoid(x)
+    print(f"y = {y}, dy = {dy}")
+    y = relu(x)
+    dy = d_relu(x)
+    print(f"y = {y}, dy = {dy}")
+    y = tanh(x)
+    dy = d_tanh(x)
+    print(f"y = {y}, dy = {dy}")
     print("Hello from learning-representations-by-backpropagating-errors!")
 
 
--- Learning representations by backpropagating errors/utils/activations.py
+++ Learning representations by backpropagating errors/utils/activations.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 # Activation functions
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
