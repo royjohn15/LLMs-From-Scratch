b1e234a382a060ccb4873506cabf06b5a268cec1
royjohn15 <royjohnabc@gmail.com>
2026-08-18 13:04:51 +0530

    added the utils folder

---
+++ Learning representations by backpropagating errors/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from learning-representations-by-backpropagating-errors!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ Learning representations by backpropagating errors/utils/activations.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+# Activation functions
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def tanh(x):
+    return np.tanh(x)
+
+def relu(x):
+    return np.maximum(0, x)
+
+# Derivative of activation functions
+def d_sigmoid(x):
+    return sigmoid(x) * (1 - sigmoid(x))
+
+def d_tanh(x):
+    return 1 - np.tanh(x)**2
+
+def d_relu(x):
+    return np.where(x > 0, 1, 0)
